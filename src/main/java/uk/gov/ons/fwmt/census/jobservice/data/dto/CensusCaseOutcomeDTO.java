2617175ac0b1719c858967cb2939920dfc5015ac
garethTurner <gareth.turner@ons.gov.uk>
2018-12-07 15:38:33 +0000

    change tests to no longer expect soap request

--- src/main/java/uk/gov/ons/fwmt/census/jobservice/data/dto/CensusCaseOutcomeDTO.java
+++ src/main/java/uk/gov/ons/fwmt/census/jobservice/data/dto/CensusCaseOutcomeDTO.java
@@ -1,8 +1,12 @@
 package uk.gov.ons.fwmt.census.jobservice.data.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CensusCaseOutcomeDTO {
   private String caseId;
   private String caseReference;
