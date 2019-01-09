c36db7404cc024695b4a0a45fd92d0a4bf9233b8
ThomasPootQA <thomas.poot@qa.com>
2019-01-09 10:56:37 +0000

    FWMT-666 - Altered the household converter to use the additional property 'caseId' as the ID

--- src/main/java/uk/gov/ons/fwmt/census/jobservice/converter/impl/HouseholdConverter.java
+++ src/main/java/uk/gov/ons/fwmt/census/jobservice/converter/impl/HouseholdConverter.java
@@ -22,7 +22,7 @@ public class HouseholdConverter implements CometConverter {
   public ModelCase convert(FWMTCreateJobRequest ingest) {
     ModelCase modelCase = new ModelCase();
     Instant instant = Instant.now();
-    modelCase.setId(ingest.getJobIdentity());
+    modelCase.setId(ingest.getAdditionalProperties().get("caseId"));
     modelCase.setReference(ingest.getJobIdentity());
     modelCase.setCaseType(CASE_TYPE_HH);
     modelCase.setState(OPEN);
@@ -35,7 +35,7 @@ public class HouseholdConverter implements CometConverter {
     modelCase.setContact(contact);
 
     Address address = new Address();
-    address.setUprn(0l);
+    address.setUprn(0L);
     address.setLines(addAddressLines(ingest));
     address.setPostCode(ingest.getAddress().getPostCode());
     modelCase.setAddress(address);
