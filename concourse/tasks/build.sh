8aa5ca9c39c167f7ad0631d98fe6e577ff01421b
Phil Sharland <philip.sharland@ons.gov.uk>
2019-01-15 13:14:21 +0000

    FWMT-675 check folder contents

--- concourse/tasks/build.sh
+++ concourse/tasks/build.sh
@@ -3,4 +3,4 @@ set -e
 fwmt-census-job-service/gradlew --build-file fwmt-census-job-service/build.gradle build --stacktrace
 cp fwmt-census-job-service/build/libs/*.jar build-output
 cd fwmt-census-job-service/build/libs
-lsd
+ls
