2f11f6297f1480700f6641ed443f7756efaded5b
Phil Sharland <philip.sharland@ons.gov.uk>
2019-01-15 07:54:22 +0000

    FWMT-675 add debug to build.sh

--- concourse/tasks/build.sh
+++ concourse/tasks/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-fwmt-census-job-service/gradlew --build-file fwmt-census-job-service/build.gradle build
+fwmt-census-job-service/gradlew --build-file fwmt-census-job-service/build.gradle build --stacktrace --debug
 cp fwmt-census-job-service/build/libs/*.jar build-output
