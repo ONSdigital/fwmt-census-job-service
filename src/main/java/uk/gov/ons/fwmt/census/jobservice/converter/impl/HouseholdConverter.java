88468177d78ad902615532a4b0cf1835f21d5471
garethTurner <gareth.turner@ons.gov.uk>
2019-02-07 13:32:40 +0000

    FWMT-712 get case id directly instead of from additional property

--- src/main/java/uk/gov/ons/fwmt/census/jobservice/converter/impl/HouseholdConverter.java
+++ src/main/java/uk/gov/ons/fwmt/census/jobservice/converter/impl/HouseholdConverter.java
@@ -22,7 +22,7 @@ public class HouseholdConverter implements CometConverter {
   public ModelCase convert(CreateFieldWorkerJobRequest ingest) {
     ModelCase modelCase = new ModelCase();
     Instant instant = Instant.now();
-    modelCase.setId(ingest.getAdditionalProperties().get("caseId"));
+    modelCase.setId(ingest.getCaseId());
     modelCase.setReference(ingest.getJobIdentity());
     modelCase.setCaseType(CASE_TYPE_HH); 
     modelCase.setState(OPEN);
