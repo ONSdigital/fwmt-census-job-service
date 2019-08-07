08bb51fbb7562ca9b88994c58342731a2d09ebbc
Simon <sdiaz1000@gmail.com>
2019-08-07 18:53:05 +0100

    added splunk functionality

--- src/main/java/uk/gov/ons/census/fwmt/jobservice/config/GatewayEventsConfig.java
+++ src/main/java/uk/gov/ons/census/fwmt/jobservice/config/GatewayEventsConfig.java
@@ -3,26 +3,34 @@ package uk.gov.ons.census.fwmt.jobservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
+import uk.gov.ons.census.fwmt.jobservice.Application;
 
 @Configuration
 public class GatewayEventsConfig {
 
-  public static final String CANONICAL_CREATE_JOB_RECEIVED = "Canonical - Create Job Received";
-  public static final String CANONICAL_CANCEL_RECEIVED = "Canonical - Cancel Job Received";
-  public static final String CANONICAL_UPDATE_RECEIVED = "Canonical - Update Job Received";
-  public static final String COMET_CREATE_SENT = "Comet - Create Job Request";
-  public static final String COMET_CREATE_ACK = "Comet - Create Job Acknowledged";
-  public static final String COMET_CANCEL_SENT = "Comet - Cancel Job Request";
-  public static final String COMET_CANCEL_ACK = "Comet - Cancel Job Acknowledged";
-  public static final String COMET_UPDATE_SENT = "Comet - Update Job Request";
-  public static final String COMET_UPDATE_ACK = "Comet - Update Job Acknowledged";
+  public static final String CANONICAL_CREATE_JOB_RECEIVED = "CANONICAL_CREATE_JOB_RECEIVED";
+  public static final String CANONICAL_CANCEL_RECEIVED = "CANONICAL_CANCEL_RECEIVED";
+  public static final String CANONICAL_UPDATE_RECEIVED = "CANONICAL_UPDATE_RECEIVED";
+  public static final String COMET_CREATE_SENT = "COMET_CREATE_SENT";
+  public static final String COMET_CREATE_ACK = "COMET_CREATE_ACK";
+  public static final String COMET_CANCEL_SENT = "COMET_CANCEL_SENT";
+  public static final String COMET_CANCEL_ACK = "COMET_CANCEL_ACK";
+  public static final String COMET_UPDATE_SENT = "COMET_UPDATE_SENT";
+  public static final String COMET_UPDATE_ACK = "COMET_UPDATE_ACK";
+
+  public static final String FAILED_TO_UNMARSHALL_CANONICAL = "FAILED_TO_UNMARSHALL_CANONICAL";
+  public static final String INVALID_CANONICAL_ACTION = "INVALID_CANONICAL_ACTION";
+  public static final String FAILED_TM_AUTHENTICATION = "FAILED_TM_AUTHENTICATION";
 
   @Bean
   public GatewayEventManager gatewayEventManager() {
     GatewayEventManager gatewayEventManager = new GatewayEventManager();
+    gatewayEventManager.setSource(Application.APPLICATION_NAME);
     gatewayEventManager.addEventTypes(new String[] {CANONICAL_CREATE_JOB_RECEIVED, CANONICAL_CANCEL_RECEIVED,
         CANONICAL_UPDATE_RECEIVED, COMET_CREATE_SENT, COMET_CREATE_ACK, COMET_CANCEL_SENT, COMET_CANCEL_ACK,
         COMET_UPDATE_SENT, COMET_UPDATE_ACK});
+    gatewayEventManager.addErrorEventTypes(new String[] {FAILED_TO_UNMARSHALL_CANONICAL, INVALID_CANONICAL_ACTION, FAILED_TM_AUTHENTICATION});
+
     return gatewayEventManager;
   }
 }
--- src/main/java/uk/gov/ons/census/fwmt/jobservice/message/GatewayActionsReceiver.java
+++ src/main/java/uk/gov/ons/census/fwmt/jobservice/message/GatewayActionsReceiver.java
@@ -1,24 +1,26 @@
 package uk.gov.ons.census.fwmt.jobservice.message;
 
+import static uk.gov.ons.census.fwmt.jobservice.config.GatewayEventsConfig.CANONICAL_CANCEL_RECEIVED;
+import static uk.gov.ons.census.fwmt.jobservice.config.GatewayEventsConfig.CANONICAL_CREATE_JOB_RECEIVED;
+import static uk.gov.ons.census.fwmt.jobservice.config.GatewayEventsConfig.CANONICAL_UPDATE_RECEIVED;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import uk.gov.ons.census.fwmt.canonical.v1.CancelFieldWorkerJobRequest;
 import uk.gov.ons.census.fwmt.canonical.v1.CreateFieldWorkerJobRequest;
 import uk.gov.ons.census.fwmt.canonical.v1.UpdateFieldWorkerJobRequest;
 import uk.gov.ons.census.fwmt.common.error.GatewayException;
 import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
+import uk.gov.ons.census.fwmt.jobservice.config.GatewayEventsConfig;
 import uk.gov.ons.census.fwmt.jobservice.service.JobService;
-
-import java.io.IOException;
-import java.time.LocalTime;
-
-import static uk.gov.ons.census.fwmt.jobservice.config.GatewayEventsConfig.CANONICAL_CANCEL_RECEIVED;
-import static uk.gov.ons.census.fwmt.jobservice.config.GatewayEventsConfig.CANONICAL_CREATE_JOB_RECEIVED;
-import static uk.gov.ons.census.fwmt.jobservice.config.GatewayEventsConfig.CANONICAL_UPDATE_RECEIVED;
 
 @Slf4j
 @Component
@@ -41,24 +43,20 @@ public class GatewayActionsReceiver {
   private void processMessage(String message) throws GatewayException {
     if (message.contains("Create")) {
       CreateFieldWorkerJobRequest fwmtCreateJobRequest = convertMessageToDTO(CreateFieldWorkerJobRequest.class, message);
-      gatewayEventManager.triggerEvent(String.valueOf(fwmtCreateJobRequest.getCaseId()), CANONICAL_CREATE_JOB_RECEIVED,
-          LocalTime.now());
+      gatewayEventManager.triggerEvent(String.valueOf(fwmtCreateJobRequest.getCaseId()), CANONICAL_CREATE_JOB_RECEIVED);
       jobService.createJob(fwmtCreateJobRequest);
-      log.info("Sending Create job to TM");
     } else if (message.contains("Cancel")) {
       CancelFieldWorkerJobRequest fwmtCancelJobRequest = convertMessageToDTO(CancelFieldWorkerJobRequest.class, message);
-      gatewayEventManager
-          .triggerEvent(String.valueOf(fwmtCancelJobRequest.getCaseId()), CANONICAL_CANCEL_RECEIVED, LocalTime.now());
+      gatewayEventManager.triggerEvent(String.valueOf(fwmtCancelJobRequest.getCaseId()), CANONICAL_CANCEL_RECEIVED);
       jobService.cancelJob(fwmtCancelJobRequest);
-      log.info("Sending Cancel job to TM");
     } else if (message.contains("update")) {
       UpdateFieldWorkerJobRequest fwmtUpdateJobRequest = convertMessageToDTO(UpdateFieldWorkerJobRequest.class, message);
-      gatewayEventManager
-          .triggerEvent(String.valueOf(fwmtUpdateJobRequest.getCaseId()), CANONICAL_UPDATE_RECEIVED, LocalTime.now());
+      gatewayEventManager.triggerEvent(String.valueOf(fwmtUpdateJobRequest.getCaseId()), CANONICAL_UPDATE_RECEIVED);
       jobService.updateJob(fwmtUpdateJobRequest);
-      log.info("Sending Update job to TM");
     } else {
-      throw new GatewayException(GatewayException.Fault.BAD_REQUEST, "Cannot process message.");
+      String errorMsg = "Invalid Canonical Action.";
+      gatewayEventManager.triggerErrorEvent(this.getClass(), errorMsg, "<UNKNOWN_CASE_ID>", GatewayEventsConfig.INVALID_CANONICAL_ACTION);
+      throw new GatewayException(GatewayException.Fault.BAD_REQUEST, errorMsg);
     }
   }
 
@@ -69,6 +67,8 @@ public class GatewayActionsReceiver {
     try {
       dto = mapper.readValue(message, klass);
     } catch (IOException e) {
+      String errorMsg = "Failed to unmarshall Canonical Action.";
+      gatewayEventManager.triggerErrorEvent(this.getClass(), errorMsg, "<UNKNOWN_CASE_ID>", GatewayEventsConfig.FAILED_TO_UNMARSHALL_CANONICAL);
       throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR, "Failed to convert message into DTO.", e);
     }
     return dto;
