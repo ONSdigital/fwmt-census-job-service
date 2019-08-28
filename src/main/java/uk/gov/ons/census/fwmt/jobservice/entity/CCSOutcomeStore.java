fd7ea072bd9f8bd1d3622521d6450d368081a139
SteveScorfield <steve.scorfield@ons.gov.uk>
2019-08-28 14:12:34 +0100

    Moved caching over to CCSOutcomeStore and removed cache related service classes

--- src/main/java/uk/gov/ons/census/fwmt/jobservice/entity/CCSOutcomeStore.java
+++ src/main/java/uk/gov/ons/census/fwmt/jobservice/entity/CCSOutcomeStore.java
@@ -1,17 +1,24 @@
 package uk.gov.ons.census.fwmt.jobservice.entity;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import uk.gov.ons.census.fwmt.jobservice.service.JobCacheManager;
+import uk.gov.ons.census.fwmt.jobservice.config.RedisUtil;
 
-
+@Slf4j
 @Component
 public class CCSOutcomeStore {
 
-  @Autowired
-  private JobCacheManager jobCacheManager;
+    @Autowired
+    private RedisUtil<String> redisUtil;
+
 
   public String retrieveCache(String caseId) {
-    return jobCacheManager.getCachedCCSOutcome(caseId);
+
+      String output = String.valueOf(redisUtil.getValue(caseId));
+      if (output != null) {
+          log.info("Received object from cache with case ID: " + caseId);
+      }
+      return output;
   }
 }
--- src/main/java/uk/gov/ons/census/fwmt/jobservice/service/JobCacheManager.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.ons.census.fwmt.jobservice.service;
-
-public interface JobCacheManager {
-
-  String getCachedCCSOutcome(String caseId);
-
-}
