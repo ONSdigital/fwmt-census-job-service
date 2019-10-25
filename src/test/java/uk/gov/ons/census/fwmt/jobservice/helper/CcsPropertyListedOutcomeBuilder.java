ee064f0d56a99fccd00e0273d287bd5b5b51efdd
jacobharrison <jacob.harrison@qa.com>
2019-10-25 13:51:42 +0100

    fix and add unit test

--- src/test/java/uk/gov/ons/census/fwmt/jobservice/helper/CcsPropertyListedOutcomeBuilder.java
+++ src/test/java/uk/gov/ons/census/fwmt/jobservice/helper/CcsPropertyListedOutcomeBuilder.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class CcsPropertyListedOutcomeBuilder {
 
-  public CCSPropertyListingOutcome createCcsPropertyListingOutcome() {
+  public CCSPropertyListingOutcome createCcsPropertyListingCeOutcome() {
     CCSPropertyListingOutcome ccsPropertyListingOutcome = new CCSPropertyListingOutcome();
 
     ccsPropertyListingOutcome.setUsername("TestMand");
@@ -22,7 +22,30 @@ public class CcsPropertyListedOutcomeBuilder {
     ceDetails.setUsualResidents(10);
     ceDetails.setBedspaces(15);
     ceDetails.setContactPhone("0123456789");
+    ceDetails.setEstablishmentType("CE");
     ccsPropertyListingOutcome.setCeDetails(ceDetails);
+
+    Address address = new Address();
+    address.setOa("1234");
+    ccsPropertyListingOutcome.setAddress(address);
+
+    ccsPropertyListingOutcome.setAccessInfo("Use gate");
+    CareCode careCode = new CareCode();
+    careCode.setCareCode("Mad dog");
+
+    List<CareCode> careCodes = new ArrayList<>();
+    careCodes.add(careCode);
+    ccsPropertyListingOutcome.setCareCodes(careCodes);
+
+    return ccsPropertyListingOutcome;
+  }
+
+  public CCSPropertyListingOutcome createCcsPropertyListingOutcome() {
+    CCSPropertyListingOutcome ccsPropertyListingOutcome = new CCSPropertyListingOutcome();
+
+    ccsPropertyListingOutcome.setUsername("TestMand");
+    ccsPropertyListingOutcome.setPrimaryOutcome("Household");
+    ccsPropertyListingOutcome.setSecondaryOutcome("Contact made");
 
     Address address = new Address();
     address.setOa("1234");
