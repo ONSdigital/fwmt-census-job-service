7fd80e824501046954eb51fa24189913d16852e1
ThomasPoot <35296806+thomaspoot@users.noreply.github.com>
2019-10-31 11:17:27 +0000

    Refactored creation of field worker update/other jobs, in tests.

--- src/test/java/uk/gov/ons/census/fwmt/jobservice/helper/FieldWorkerJobRequestBuilder.java
+++ src/test/java/uk/gov/ons/census/fwmt/jobservice/helper/FieldWorkerJobRequestBuilder.java
@@ -8,11 +8,36 @@ import java.util.UUID;
 
 public class FieldWorkerJobRequestBuilder {
 
+  private static UUID EXAMPLE_CASE_ID = UUID.fromString("a48bf28e-e7f4-4467-a9fb-e000b6a55676");
+
+  private Address createNewAddress() {
+    Address address = new Address();
+
+    address.setLine1("1 Station Road");
+    address.setTownName("Town");
+    address.setPostCode("AB1 2CD");
+    address.setLatitude(BigDecimal.valueOf(1234.56));
+    address.setLongitude(BigDecimal.valueOf(2345.67));
+    address.setOa("oaTest");
+
+    return address;
+  }
+
+  private Address createCCSNewAddress() {
+    Address address = new Address();
+
+    address.setPostCode("AB1 2CD");
+    address.setLatitude(BigDecimal.valueOf(1234.56));
+    address.setLongitude(BigDecimal.valueOf(2345.67));
+
+    return address;
+  }
+
   private CreateFieldWorkerJobRequest createDefaultCreateRequest() {
     CreateFieldWorkerJobRequest request = new CreateFieldWorkerJobRequest();
 
     request.setActionType("Create");
-    request.setCaseId(UUID.fromString("a48bf28e-e7f4-4467-a9fb-e000b6a55676"));
+    request.setCaseId(EXAMPLE_CASE_ID);
     request.setCaseReference("caseReference");
     request.setCaseType("Household");
     request.setEstablishmentType("estabType");
@@ -51,10 +76,9 @@ public class FieldWorkerJobRequestBuilder {
 
   public CreateFieldWorkerJobRequest createFieldWorkerCCSIVJobRequestForConvert() {
     CreateFieldWorkerJobRequest request = createDefaultCreateRequest();
+    // TODO this used to be set, then later overridden by the default. Commented for posterity.
+    // request.setMandatoryResource("ccsInterviewer");
     request.setCaseType("CCS");
-    request.setMandatoryResource("ccsInterviewer");
-    request.setEstablishmentType(null);
-    request.setMandatoryResource("TestMand");
     request.setEstablishmentType(null);
     request.setAddress(createCCSNewAddress());
     request.setContact(null);
@@ -64,8 +88,8 @@ public class FieldWorkerJobRequestBuilder {
   public CreateFieldWorkerJobRequest createFieldWorkerCCSPLJobRequestForConvert() {
     CreateFieldWorkerJobRequest request = createDefaultCreateRequest();
     request.setCaseReference("123456789");
-    request.setMandatoryResource("ccsInterviewer");
-    request.setMandatoryResource("TestMand");
+    // TODO this used to be set, then later overridden by the default. Commented for posterity.
+    // request.setMandatoryResource("ccsInterviewer");
     request.setCaseType(null);
     request.setEstablishmentType(null);
     request.setAddress(createCCSNewAddress());
@@ -76,7 +100,7 @@ public class FieldWorkerJobRequestBuilder {
   public CancelFieldWorkerJobRequest cancelFieldWorkerJobRequest() {
     CancelFieldWorkerJobRequest cancelFieldWorkerJobRequest = new CancelFieldWorkerJobRequest();
 
-    cancelFieldWorkerJobRequest.setCaseId(UUID.fromString("a48bf28e-e7f4-4467-a9fb-e000b6a55676"));
+    cancelFieldWorkerJobRequest.setCaseId(EXAMPLE_CASE_ID);
     cancelFieldWorkerJobRequest.setReason("TestReason");
     cancelFieldWorkerJobRequest.setActionType("actionType");
     cancelFieldWorkerJobRequest.setUntil(OffsetDateTime.parse("2030-01-01T00:00+00:00"));
@@ -84,33 +108,10 @@ public class FieldWorkerJobRequestBuilder {
     return cancelFieldWorkerJobRequest;
   }
 
-  private Address createNewAddress() {
-    Address address = new Address();
-
-    address.setLine1("1 Station Road");
-    address.setTownName("Town");
-    address.setPostCode("AB1 2CD");
-    address.setLatitude(BigDecimal.valueOf(1234.56));
-    address.setLongitude(BigDecimal.valueOf(2345.67));
-    address.setOa("oaTest");
-
-    return address;
-  }
-
-  private Address createCCSNewAddress() {
-    Address address = new Address();
-
-    address.setPostCode("AB1 2CD");
-    address.setLatitude(BigDecimal.valueOf(1234.56));
-    address.setLongitude(BigDecimal.valueOf(2345.67));
-
-    return address;
-  }
-
   public UpdateFieldWorkerJobRequest updateFieldWorkerJobRequestWithPause() {
     UpdateFieldWorkerJobRequest updateFieldWorkerJobRequest = new UpdateFieldWorkerJobRequest();
 
-    updateFieldWorkerJobRequest.setCaseId(UUID.fromString("a48bf28e-e7f4-4467-a9fb-e000b6a55676"));
+    updateFieldWorkerJobRequest.setCaseId(EXAMPLE_CASE_ID);
     updateFieldWorkerJobRequest.setAddressType("HH");
     updateFieldWorkerJobRequest.setActionType("update");
     updateFieldWorkerJobRequest.setUaa(false);
@@ -123,7 +124,7 @@ public class FieldWorkerJobRequestBuilder {
   public UpdateFieldWorkerJobRequest updateFieldWorkerJobRequestReinstate() {
     UpdateFieldWorkerJobRequest updateFieldWorkerJobRequest = new UpdateFieldWorkerJobRequest();
 
-    updateFieldWorkerJobRequest.setCaseId(UUID.fromString("a48bf28e-e7f4-4467-a9fb-e000b6a55676"));
+    updateFieldWorkerJobRequest.setCaseId(EXAMPLE_CASE_ID);
     updateFieldWorkerJobRequest.setAddressType("HH");
     updateFieldWorkerJobRequest.setActionType("update");
     updateFieldWorkerJobRequest.setUaa(false);
@@ -134,39 +135,27 @@ public class FieldWorkerJobRequestBuilder {
   }
 
   public CreateFieldWorkerJobRequest createFieldWorkerJobRequestForCCSIVConvert() {
-    Address address = new Address();
-    CreateFieldWorkerJobRequest createFieldWorkerJobRequest = new CreateFieldWorkerJobRequest();
+    CreateFieldWorkerJobRequest createFieldWorkerJobRequest = createDefaultCreateRequest();
 
-    createFieldWorkerJobRequest.setActionType("Create");
     createFieldWorkerJobRequest.setCaseReference("Test");
     createFieldWorkerJobRequest.setSurveyType("CCSIV");
     createFieldWorkerJobRequest.setEstablishmentType("Household");
     createFieldWorkerJobRequest.setMandatoryResource("Test_123");
     createFieldWorkerJobRequest.setCoordinatorId("123_abc");
 
-    address.setOa("!23_abc");
-    address.setLine1("1");
-    address.setLine2("Station Road");
-    address.setTownName("Chingford");
-    address.setPostCode("E4 7NG");
-    address.setLatitude(BigDecimal.valueOf(51));
-    address.setLongitude(BigDecimal.valueOf(0.0001));
-    createFieldWorkerJobRequest.setAddress(address);
+    // TODO this used to be set, then later overridden by the default. Commented for posterity.
+    // Address address = new Address();
+    // address.setOa("!23_abc");
+    // address.setLine1("1");
+    // address.setLine2("Station Road");
+    // address.setTownName("Chingford");
+    // address.setPostCode("E4 7NG");
+    // address.setLatitude(BigDecimal.valueOf(51));
+    // address.setLongitude(BigDecimal.valueOf(0.0001));
+    // createFieldWorkerJobRequest.setAddress(address);
 
-    createFieldWorkerJobRequest.setCaseId(UUID.fromString("a48bf28e-e7f4-4467-a9fb-e000b6a55676"));
-    createFieldWorkerJobRequest.setCaseReference("caseReference");
-    createFieldWorkerJobRequest.setCaseType("Household");
     createFieldWorkerJobRequest.setCcsQuestionnaireURL("1133244");
     createFieldWorkerJobRequest.setSpecialInstructions("Test");
-
-    createFieldWorkerJobRequest.setAddress(createNewAddress());
-
-    Contact contact = new Contact();
-
-    contact.setOrganisationName("Test");
-    contact.setEmailAddress("Test@test.co.uk");
-    contact.setPhoneNumber("07123456789");
-    createFieldWorkerJobRequest.setContact(contact);
 
     return createFieldWorkerJobRequest;
   }
