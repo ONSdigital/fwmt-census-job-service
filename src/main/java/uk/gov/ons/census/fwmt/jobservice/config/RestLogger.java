50c00ed7c4c7b2d4308a32af7b0fa90e14bc9128
ThomasPoot <35296806+thomaspoot@users.noreply.github.com>
2019-10-31 09:36:11 +0000

    Removed a use of Google's 'Bytestreams', which is marked as unstable Ensured non-nullability to match the API we're overriding in RestLogger

--- src/main/java/uk/gov/ons/census/fwmt/jobservice/config/RestLogger.java
+++ src/main/java/uk/gov/ons/census/fwmt/jobservice/config/RestLogger.java
@@ -1,21 +1,23 @@
 package uk.gov.ons.census.fwmt.jobservice.config;
 
-import com.google.common.io.ByteStreams;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.lang.NonNull;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 @Slf4j
 public class RestLogger implements ClientHttpRequestInterceptor {
 
   @Override
-  public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution)
+  @NonNull
+  public ClientHttpResponse intercept(@NonNull HttpRequest request, @NonNull byte[] body,
+      ClientHttpRequestExecution execution)
       throws IOException {
     ClientHttpResponse response = execution.execute(request, body);
 
@@ -27,7 +29,7 @@ public class RestLogger implements ClientHttpRequestInterceptor {
         new String(body, StandardCharsets.UTF_8),
         response.getStatusCode(),
         response.getHeaders(),
-        new String(ByteStreams.toByteArray(response.getBody()), StandardCharsets.UTF_8));
+        new String(IOUtils.toByteArray(response.getBody()), StandardCharsets.UTF_8));
 
     return response;
   }
